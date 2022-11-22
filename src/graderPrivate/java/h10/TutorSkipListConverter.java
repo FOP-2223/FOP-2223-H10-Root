e6269318898024f4dc051d9c19e70ca447531c01
Nyanyan <46193012+zentox@users.noreply.github.com>
2022-11-22 20:17:52 +0100

    tutor skiplist to track the number of comparisons on each operation

---
+++ src/graderPrivate/java/h10/TutorSkipListConverter.java
@@ -0,0 +1,29 @@
+package h10;
+
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+/**
+ * Converter for converting a JSON object to a {@link TutorSkipList}.
+ *
+ * @author Nhan Huynh
+ */
+public class TutorSkipListConverter implements ArgumentConverter {
+
+    /**
+     * Converts the given source to an object of the type of the parameter.
+     */
+    private final SkipListConverter converter = new SkipListConverter();
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+        SkipList<Integer> list = (SkipList<Integer>) converter.convert(source, context);
+        TutorSkipList<Integer> tutorList = new TutorSkipList<>(list.cmp, list.getMaxLevel(), list.getProbability());
+        tutorList.head = list.head;
+        tutorList.currentMaxLevel = list.currentMaxLevel;
+        return tutorList;
+    }
+
+}
