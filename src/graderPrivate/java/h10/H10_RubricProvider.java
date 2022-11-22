f60bafc8b6b19a5e3bd843badc81dd666975b6ae
Nyanyan <46193012+zentox@users.noreply.github.com>
2022-11-22 20:18:25 +0100

    Update H10_RubricProvider.java

--- src/graderPrivate/java/h10/H10_RubricProvider.java
+++ src/graderPrivate/java/h10/H10_RubricProvider.java
@@ -4,7 +4,7 @@ import org.sourcegrade.jagr.api.rubric.Criterion;
 import org.sourcegrade.jagr.api.rubric.Rubric;
 import org.sourcegrade.jagr.api.rubric.RubricProvider;
 
-import static h10.TutorUtils.criterion;
+import static h10.PublicTutorUtils.criterion;
 
 /**
  * Defines the private rubric for the assignment H10.
@@ -16,17 +16,17 @@ public class H10_RubricProvider implements RubricProvider {
     /**
      * Defines the private rubric for the task H1.
      */
-    private static final Criterion H1 = criterion("H1 | SkipList#contains(Object)", H1_Tests.class);
+    private static final Criterion H1 = criterion("H1 | SkipList#contains(Object)", H1_PrivateTests.class);
 
     /**
      * Defines the private rubric for the task H2.
      */
-    private static final Criterion H2 = criterion("H2 | SkipList#add(Object)", H2_Tests.class);
+    private static final Criterion H2 = criterion("H2 | SkipList#add(Object)", H2_PrivateTests.class);
 
     /**
      * Defines the private rubric for the task H3.
      */
-    private static final Criterion H3 = criterion("H3 | SkipList#remove(Object)", H3_Tests.class);
+    private static final Criterion H3 = criterion("H3 | SkipList#remove(Object)", H3_PrivateTests.class);
 
     @Override
     public Rubric getRubric() {
