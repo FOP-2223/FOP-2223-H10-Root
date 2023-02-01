34a90c4a662b0fc1d564eb8377e0b850f17908ef
nyanyan <46193012+zentox@users.noreply.github.com>
2023-02-01 11:00:23 +0100

    Rework tests for H2 using the comparison check

--- src/graderPrivate/java/h10/H2_PrivateTests.java
+++ src/graderPrivate/java/h10/H2_PrivateTests.java
@@ -11,6 +11,8 @@ import org.tudalgo.algoutils.tutor.general.conversion.ArrayConverter;
 
 import java.util.List;
 
+import static h10.PrivateTutorUtils.assertComparisons;
+import static h10.PrivateTutorUtils.convert;
 import static h10.PublicTutorUtils.PROBABILITY_ALWAYS_ADD;
 import static h10.PublicTutorUtils.contextH2;
 import static h10.PublicTutorUtils.listItemAsList;
@@ -24,52 +26,46 @@ import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.assertE
  */
 @DisplayName("H2")
 @TestForSubmission
-@SuppressWarnings({"unchecked", "DuplicatedCode"})
 public class H2_PrivateTests {
 
     /**
-     * Tests if the {@link SkipList#add(Object)}  method adds element on  levels correctly and if the number of
+     * Tests if the {@link SkipList#add(Object)}  method adds element on levels correctly and if the patg of
      * comparison to find the correct insertion point is correct.
      *
      * @param object                the list to test
      * @param key                   the element to add
-     * @param comparison            the number of comparisons to find the correct insertion point
+     * @param comparisons           the expected comparisons path
      * @param numberOfElementsLevel the number of elements on each level
      * @param refs                  the indices of the added elements
      */
-    @SuppressWarnings("unchecked")
-    private void assertAddOnLevels(Object object, Integer key, int comparison, Integer[] numberOfElementsLevel,
+    private void assertAddOnLevels(Object object, Integer key, Integer[][] comparisons, Integer[] numberOfElementsLevel,
                                    Integer[] refs) {
-        TutorSkipList<Integer> list = (TutorSkipList<Integer>) object;
+        VisitorSkipList<Integer> list = convert(object);
         list.setProbability(PROBABILITY_ALWAYS_ADD);
+        List<List<ListItem<ExpressNode<VisitorNode<Integer>>>>> nodes = listItemAsList(list.head);
+        VisitorNode<Integer> toAdd = new VisitorNode<>(key);
+        Context context = contextH2(list, toAdd);
 
-        Context context = contextH2(list, key);
+        assertComparisons(nodes, comparisons, context);
 
-        assertEquals(
-            comparison,
-            list.getComparisonCount(),
-            context,
-            result -> String.format("The call to add(%d) should have made %d comparisons, but made %d instead.",
-                key, comparison, result.object())
-        );
-        List<List<ListItem<ExpressNode<Integer>>>> itemRefs = listItemAsList(list.head);
+        List<List<ListItem<ExpressNode<VisitorNode<Integer>>>>> itemRefs = listItemAsList(list.head);
 
         for (int i = 0; i < numberOfElementsLevel.length; i++) {
             int level = i;
-            List<ListItem<ExpressNode<Integer>>> node = itemRefs.get(i);
+            List<ListItem<ExpressNode<VisitorNode<Integer>>>> node = itemRefs.get(i);
             assertEquals(
                 numberOfElementsLevel[i] + 1,
                 node.size(),
                 context,
-                result -> String.format("The call to add(%d) should have contain %d elements on level %d, but given %d "
-                    + "instead.", key, numberOfElementsLevel[level] + 1, level, result.object())
+                result -> String.format("The call to add(%s) should have contain %d elements on level %d, but given %d "
+                    + "instead.", toAdd, numberOfElementsLevel[level] + 1, level, result.object())
             );
             assertEquals(
-                key,
+                toAdd,
                 node.get(refs[i] + 1).key.value,
                 context,
-                result -> String.format("The call to add(%d) should have added %d on level %d, but added %d instead.",
-                    key, key, level, result.object())
+                result -> String.format("The call to add(%s) should have added %s on level %d, but added %s instead.",
+                    toAdd, toAdd, level, result.object())
             );
         }
     }
@@ -87,7 +83,7 @@ public class H2_PrivateTests {
      *             ("maxHeight": integer)
      *         }
      *         "key": integer,
-     *         "comparison": integer,
+     *         "comparisons": 2D array of integers,
      *         "numberOfElementsLevel": array of integers,
      *         "refs": array of integers
      *     }
@@ -95,7 +91,7 @@ public class H2_PrivateTests {
      *
      * @param object                the list to test
      * @param key                   the element to add
-     * @param comparison            the number of comparisons to find the correct insertion point
+     * @param comparisons           the expected comparisons path
      * @param numberOfElementsLevel the number of elements on each level
      * @param refs                  the indices of the added elements
      */
@@ -103,13 +99,26 @@ public class H2_PrivateTests {
     @ParameterizedTest(name = "Test {index}: Einfügen von Element [1} mit minimalen Vergleich {2}.")
     @JsonClasspathSource("h2/create_new_levels.json")
     public void testAddCreateNewLevels(
-        @Property("list") @ConvertWith(TutorSkipListConverter.class) Object object,
+        @Property("list") @ConvertWith(VisitorSkipListConverter.class) Object object,
         @Property("key") Integer key,
-        @Property("comparison") int comparison,
+        @Property("comparisons") @ConvertWith(ArrayConverter.Auto.class) Integer[][] comparisons,
         @Property("numberOfElementsLevel") @ConvertWith(ArrayConverter.Auto.class) Integer[] numberOfElementsLevel,
         @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs
     ) {
-        assertAddOnLevels(object, key, comparison, numberOfElementsLevel, refs);
+        assertAddOnLevels(object, key, comparisons, numberOfElementsLevel, refs);
+    }
+
+    @DisplayName("13 | Methode fügt Element in vorhandenen Ebenen korrekt.")
+    @ParameterizedTest(name = "Test {index}: Einfügen von Element [1} mit minimalen Vergleich {2}.")
+    @JsonClasspathSource("h2/on_each_level.json")
+    public void testAddOnEachLevel(
+        @Property("list") @ConvertWith(VisitorSkipListConverter.class) Object object,
+        @Property("key") Integer key,
+        @Property("comparisons") @ConvertWith(ArrayConverter.Auto.class) Integer[][] comparisons,
+        @Property("numberOfElementsLevel") @ConvertWith(ArrayConverter.Auto.class) Integer[] numberOfElementsLevel,
+        @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs
+    ) {
+        assertAddOnLevels(object, key, comparisons, numberOfElementsLevel, refs);
     }
 
     /**
@@ -125,7 +134,7 @@ public class H2_PrivateTests {
      *             ("maxHeight": integer)
      *         }
      *         "key": integer,
-     *         "comparison": integer,
+     *         "comparisons": 2D array of integers,
      *         "numberOfElementsLevel": array of integers,
      *         "refs": array of integers
      *     }
@@ -133,21 +142,21 @@ public class H2_PrivateTests {
      *
      * @param object                the list to test
      * @param key                   the element to add
-     * @param comparison            the number of comparisons to find the correct insertion point
+     * @param comparisons           the expected comparisons path
      * @param numberOfElementsLevel the number of elements on each level
      * @param refs                  the indices of the added elements
      */
-    @DisplayName("13 | Methode fügt Element in vorhandenen Ebenen korrekt.")
+    @DisplayName("14 | Methode fügt Element in allen Ebenen korrekt.")
     @ParameterizedTest(name = "Test {index}: Einfügen von Element [1} mit minimalen Vergleich {2}.")
-    @JsonClasspathSource("h2/on_each_level.json")
-    public void testAddOnEachLevel(
-        @Property("list") @ConvertWith(TutorSkipListConverter.class) Object object,
+    @JsonClasspathSource("h2/on_each_level_max.json")
+    public void testAddOnEachLevelMax(
+        @Property("list") @ConvertWith(VisitorSkipListConverter.class) Object object,
         @Property("key") Integer key,
-        @Property("comparison") int comparison,
+        @Property("comparisons") @ConvertWith(ArrayConverter.Auto.class) Integer[][] comparisons,
         @Property("numberOfElementsLevel") @ConvertWith(ArrayConverter.Auto.class) Integer[] numberOfElementsLevel,
         @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs
     ) {
-        assertAddOnLevels(object, key, comparison, numberOfElementsLevel, refs);
+        assertAddOnLevels(object, key, comparisons, numberOfElementsLevel, refs);
     }
 
     /**
@@ -163,7 +172,7 @@ public class H2_PrivateTests {
      *             ("maxHeight": integer)
      *         }
      *         "key": integer,
-     *         "compare": integer,
+     *         "comparisons": 2D array of integers,
      *         "numberOfElementsLevel": array of integers,
      *         "refs": array of integers
      *     }
@@ -171,44 +180,7 @@ public class H2_PrivateTests {
      *
      * @param object                the list to test
      * @param key                   the element to add
-     * @param comparison            the number of comparisons to find the correct insertion point
-     * @param numberOfElementsLevel the number of elements on each level
-     * @param refs                  the indices of the added elements
-     */
-    @DisplayName("14 | Methode fügt Element in allen Ebenen korrekt.")
-    @ParameterizedTest(name = "Test {index}: Einfügen von Element [1} mit minimalen Vergleich {2}.")
-    @JsonClasspathSource("h2/on_each_level_max.json")
-    public void testAddOnEachLevelMax(
-        @Property("list") @ConvertWith(TutorSkipListConverter.class) Object object,
-        @Property("key") Integer key,
-        @Property("comparison") int comparison,
-        @Property("numberOfElementsLevel") @ConvertWith(ArrayConverter.Auto.class) Integer[] numberOfElementsLevel,
-        @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs
-    ) {
-        assertAddOnLevels(object, key, comparison, numberOfElementsLevel, refs);
-    }
-
-    /**
-     * Tests if the {@link SkipList#add(Object)}  method adds elements on the beginning and at the end correctly.
-     *
-     * <p>The parameters are read from the json file with the following structure:
-     *
-     * <pre>{@code
-     *     {
-     *         "list": {
-     *             "levels": 2D array of integers,
-     *             ("maxHeight": integer)
-     *         }
-     *         "key": integer,
-     *         "compare": integer,
-     *         "numberOfElementsLevel": array of integers,
-     *         "refs": array of integers
-     *     }
-     * }</pre>
-     *
-     * @param object                the list to test
-     * @param key                   the element to add
-     * @param comparison            the number of comparisons to find the correct insertion point
+     * @param comparisons           the expected comparisons path
      * @param numberOfElementsLevel the number of elements on each level
      * @param refs                  the indices of the added elements
      */
@@ -216,13 +188,13 @@ public class H2_PrivateTests {
     @ParameterizedTest(name = "Test {index}: Einfügen von Element [1} mit minimalen Vergleich {2}.")
     @JsonClasspathSource({"h2/first.json", "h2/last.json"})
     public void testAddFirstLast(
-        @Property("list") @ConvertWith(TutorSkipListConverter.class) Object object,
+        @Property("list") @ConvertWith(VisitorSkipListConverter.class) Object object,
         @Property("key") Integer key,
-        @Property("comparison") int comparison,
+        @Property("comparisons") @ConvertWith(ArrayConverter.Auto.class) Integer[][] comparisons,
         @Property("numberOfElementsLevel") @ConvertWith(ArrayConverter.Auto.class) Integer[] numberOfElementsLevel,
         @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs
     ) {
-        assertAddOnLevels(object, key, comparison, numberOfElementsLevel, refs);
+        assertAddOnLevels(object, key, comparisons, numberOfElementsLevel, refs);
     }
 
     /**
@@ -252,7 +224,7 @@ public class H2_PrivateTests {
         @Property("list") @ConvertWith(SkipListConverter.class) Object object,
         @Property("key") Integer key,
         @Property("refs") @ConvertWith(ArrayConverter.Auto.class) Integer[] refs) {
-        SkipList<Integer> list = (SkipList<Integer>) object;
+        SkipList<Integer> list = convert(object);
         list.setProbability(PROBABILITY_ALWAYS_ADD);
 
         Context context = contextH2(list, key);
